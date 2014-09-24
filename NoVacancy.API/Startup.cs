b29b4ef2a80ec6ca0d8d0819ab03eb4d753a2eee
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-09-24 15:40:43 +0400

    Install Ninject

---
+++ NoVacancy.API/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace NoVacancy.API
+{
+    //This will tell the application to run the Startup class when it starts
+    [assembly: OwinStartup(typeof(NoVacancy.API.Startup))]
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
